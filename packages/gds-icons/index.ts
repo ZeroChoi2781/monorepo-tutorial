bd167debbd0603e93563a1dcf8184faa616be59a
zero.choi <88305061+ZeroChoi2781@users.noreply.github.com>
2022-08-02 15:20:39 +0900

    test

--- packages/gds-icons/index.ts
+++ packages/gds-icons/index.ts
@@ -1,2 +1,2 @@
 export { default as Sum } from './src/sum/index';
-export { default as ActivityIcon } from './src/ActivityIcon/index';
+export { default as Icon } from './src/Icon/index';
--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,24 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import json from 'rollup-plugin-json';
+import url from 'rollup-plugin-url';
+import svgr from '@svgr/rollup';
+
 import fs from 'fs';
 import readPkgUp from 'read-pkg-up';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 const excludePath = 'node_modules/**';
 
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
+const input = 'index.ts';
+
+// 각 packages의 pcakage.json
 const { packageJson: pkg } = readPkgUp.sync({
 	cwd: fs.realpathSync(process.cwd()),
 });
-
-const input = 'index.ts';
-
-export default [
-	buildJS(input, pkg.main, 'cjs'),
-	buildJS(input, 'dist/esm', 'es'),
-];
 
 function buildJS(input, output, format) {
 	const defaultOutputConfig = {
@@ -41,22 +41,36 @@ function buildJS(input, output, format) {
 		external: ['react'],
 		output: [format === 'es' ? esOutputConfig : cjsOutputConfig],
 		plugins: [
-			peerDepsExternal(),
-			commonjs(),
 			resolve({
-				preferBuiltins: false,
 				extensions,
 			}),
 			babel({
-				presets: ['@babel/env', '@babel/preset-typescript'],
+				babelrc: false,
+				presets: [
+					'@babel/env',
+					'@babel/preset-typescript',
+					'@babel/preset-react',
+				],
 				plugins: ['@babel/plugin-transform-runtime'],
+				extensions,
 				babelHelpers: 'runtime',
 				exclude: excludePath,
-				extensions,
 			}),
+
+			// TODO: 이게 뭔지 알아보기
+			peerDepsExternal(),
+			commonjs(),
+			json(),
+			url(),
+			svgr(),
 		],
 		preserveModules: format === 'es',
 	};
 
 	return config;
 }
+
+export default [
+	buildJS(input, pkg.main, 'cjs'),
+	buildJS(input, pkg.module, 'es'),
+];
